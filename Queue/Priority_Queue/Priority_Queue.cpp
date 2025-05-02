7db01820e2a9cae09297d8c2c21fc2d035ccbe33
yumna1623 <yumnanasir1623@gmail.com>
2025-05-02 22:11:59 +0500

    Queue

---
+++ Queue/Priority_Queue/Priority_Queue.cpp
@@ -0,0 +1,61 @@
+#include <iostream>
+using namespace std;
+
+const int size = 10;
+
+struct Task {
+    string name;
+    int priority;
+};
+
+Task heap[size];
+int heapSize = 0;
+
+void insertTask() {
+    Task t;
+    cout << "Enter Task Name: ";
+    cin >> t.name;
+    cout << "Enter Task Priority (smaller number = higher priority): ";
+    cin >> t.priority;
+
+    if (heapSize == size) {
+        cout << "Heap is full. Cannot insert." << endl;
+        return;
+    }
+
+    int i = heapSize;
+    heap[i] = t;
+    heapSize++;
+
+    while (i > 0) {
+        int parent = (i - 1) / 2;
+        if (heap[i].priority < heap[parent].priority) {
+            swap(heap[i], heap[parent]);
+            i = parent;
+        } else {
+            break;
+        }
+    }
+}
+
+
+void printHeap() {
+    cout << "Min Heap (Tasks): ";
+    for (int i = 0; i < heapSize; i++) {
+        cout << "[" << heap[i].name << " " << heap[i].priority << "] ";
+    }
+    cout << endl;
+}
+
+int main() {
+    int numTasks;
+    cout << "How many tasks do you want to insert into the heap: ";
+    cin >> numTasks;
+
+    for (int i = 0; i < numTasks; i++) {
+        insertTask();
+    }
+
+    printHeap();
+}
+ 
